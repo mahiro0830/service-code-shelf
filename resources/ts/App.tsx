a6c13775f87615a93659a0d2b2fe22911ed85d04
峯岸真弘 <shishisus4@gmail.com>
2024-05-22 20:01:03 +0900

    必要パッケージのインストール

---
+++ resources/ts/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>テスト</h1>;
+}
+
+export default App;
---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <title>環境構築</title>
+    @viteReactRefresh
+    @vite([
+        'resources/scss/app.scss',
+        'resources/ts/index.tsx',
+    ])
+</head>
+<body>
+    <div id="app">
+    </div>
+</body>
+</html>
